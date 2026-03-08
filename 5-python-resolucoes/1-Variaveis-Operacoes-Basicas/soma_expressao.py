eaee141709fd25059e953c611d74edf9e7b09646
Jader Vanderlinde <jadercesarvanderlinde12@gmail.com>
2026-03-08 14:03:47 -0300

    Resolucao exercicio 1.2 (soma_variaveis)

---
+++ 5-python-resolucoes/1-Variaveis-Operacoes-Basicas/soma_expressao.py
@@ -0,0 +1,4 @@
+a = 3
+b = 5
+
+print(F'O resultado e {(2 * a) * (3 * b)}')
---
+++ 5-python-resolucoes/1-Variaveis-Operacoes-Basicas/soma_variaveis.py
@@ -0,0 +1,5 @@
+primeiraVariavel = 1
+segundaVariavel  = 2
+terceiraVariavel = 3
+
+print(f'A soma de {primeiraVariavel, segundaVariavel, terceiraVariavel} e {primeiraVariavel + segundaVariavel + terceiraVariavel}')
